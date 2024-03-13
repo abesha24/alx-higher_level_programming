de4d1ee793677bc58e602c463123708042a8e07d
abesha24 <abesha24@gmail.com>
2024-03-13 23:07:44 +0300

    0x!3 javascript

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
+const list = require('./100-data').list;
+const multiIdx = (value, index) => value * index;
+const newlist = list.map(multiIdx);
 console.log(list);
-console.log(list.map((item, index) => item * index));
+console.log(newlist);
